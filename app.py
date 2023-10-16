03e816d354b707f79c07a50ba88dae906fa275d2
Adzi-Tay Emmanuel <104699433+emmanueltay@users.noreply.github.com>
2023-10-16 08:54:29 +0000

    Add files via upload

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/fizzbuzz')
+def fizzbuzz():
+    digits = []
+    for i in range(1,101):
+        digits.append(i)
+
+    outcome = []
+
+    for digit in digits:
+        if digit % 3 == 0 and digit % 5 == 0:
+            outcome.append("FizzBuzz")
+        elif digit % 3 == 0:
+            outcome.append("Fizz")
+        elif digit % 5 == 0:
+            outcome.append("Buzz")
+        else:
+            outcome.append(digit)
+
+    return render_template('FizzBuzz_template.html', result = outcome)
+
+if __name__ == '__main__':
+    app.run()
